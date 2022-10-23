3d91dc1acb6b418487dc8e15921eda987d7f4649
juanmontenegro58 <juanmontenegro58@gmail.com>
2022-10-23 09:48:55 -0500

    Integración de estilos css para la visualización del feed de instagram

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from .config import Config
 from .models import db
@@ -6,12 +8,15 @@ from app.mysql_service import intert_producto #delete_producto,get_producto
 
 
 def create_app():
+    os.environ.setdefault('FLASK_APP', 'main.py')
+    os.environ.setdefault('FLASK_DEBUG', '1')
+    os.environ.setdefault('FLASK_ENV', 'development')
     app = Flask(__name__)
     bootstrap = Bootstrap(app)
+    app.config.from_object(Config)
 
     db.init_app(app)
 
-    app.config.from_object(Config)
 
     with app.app_context():
         db.create_all()
--- app/static/script.js
+++ app/static/script.js
@@ -1,22 +1,22 @@
 const fetchData = async () => {
-    const access_token = '';
+    const access_token = 'IGQVJWTHg3anRtOG5rMVZAfbGYzM25yeUw1YXdaby1qbDRnZAERibDlCcUhPMDVRdTAxa3FGWTZAyR01ORnBLLVZAEQ1I3aTBNaEUtWUhxY01Ed3l4bU5qRHVhd0dZAdmI3MzN3NkdZAQ0JR';
     const fields = 'id,caption,media_type,media_url,permalink'
     const url = `https://graph.instagram.com/me/media?access_token=${access_token}&fields=${fields}`
 
     const response = await fetch(url)
     const data = await response.json()
-    let content = '<div class"row">'
+    let content = ''
     for(let item of data.data) {
         const mediaType = item.media_type
         if(mediaType === 'IMAGE'){
-            content += `<div class="col-md-6">
+            content += `<div class="column">
                 <a href="${item.permalink}" target="_blank">
-                <img src="${item.media_url}" class="dimensiones_ig" alt="...">
+                <img src="${item.media_url}" class="dimensiones_ig image" alt="...">
                 </a>
             </div>`
         }
     }
-    content += '</div>'
+    content += ''
     const galeria_port = document.getElementById('galeria_port')
     galeria_port.innerHTML = content
 }
